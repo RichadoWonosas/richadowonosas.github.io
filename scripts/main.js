8e42e1b1da8277fc2b932198fc0ebf0e7b0cfb99
RichadoWonosas <richadowonosas@gmail.com>
2024-06-04 23:11:53 +0800

    Page title localized

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,11 @@ import { drawer, config } from "./drawer.js";
 import { localizeHelper } from "./localize.js";
 
 const initialise = () => {
+    localizeHelper.importTranslation("../resources/localized-strings.json");
+    localizeHelper.registerLocaleChangeCallback("title", (str) => document.title = str);
+
     config.loadGlobalConfig();
     drawer.appendToDocument();
-
-    localizeHelper.importTranslation("../resources/localized-strings.json");
 };
 
 document.body.onload = initialise;
