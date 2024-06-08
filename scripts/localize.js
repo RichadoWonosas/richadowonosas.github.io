4851e7a32b154b28649f7333af86203d38662d73
RichadoWonosas <richadowonosas@gmail.com>
2024-06-08 17:58:40 +0800

    localize revert

--- scripts/localize.js
+++ scripts/localize.js
@@ -36,13 +36,18 @@ const localizeHelper = (() => {
     );
 
     // helper functions
-    result.importTranslation = (data) => {
-        for (let i in data)
-            if (!translate[i])
-                translate[i] = data[i];
-        for (let callback in translationImported)
-            translationImported[callback]();
-    };
+    result.importTranslation = (path) =>
+        getJsonData(path).then(
+            (data) => {
+                for (let i in data)
+                    if (!translate[i])
+                        translate[i] = data[i];
+                for (let callback in translationImported)
+                    translationImported[callback]();
+            }
+        ).catch(
+            (err) => alert(err)
+        );
     result.registerTranslationImportedCallback = (onTranslationImported = () => { }) => {
         translationImported.push(onTranslationImported);
     }
