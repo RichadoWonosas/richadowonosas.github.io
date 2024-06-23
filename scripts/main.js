9262915a3997c9a6a97c6203e3c67cc483450799
RichadoWonosas <richadowonosas@gmail.com>
2024-06-23 20:03:39 +0800

    delete deprecated script

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,5 @@
 "use strict";
 
-// import {drawer, config} from "./drawer.js";
-// import {localizeHelper} from "./localize.js";
-//
-// const initialise = () => {
-//     localizeHelper.importTranslation("../resources/localized-strings.json");
-//     localizeHelper.registerLocaleChangeCallback("title", (str) => document.title = str);
-//
-//     config.loadGlobalConfig();
-//     drawer.appendToDocument();
-// };
-
 import {helper} from "./helper.js";
 
 const initialise = () => {
